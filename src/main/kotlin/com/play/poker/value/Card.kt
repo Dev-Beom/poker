2fd1f522f6fca433bf044264e491f657294e87c3
benn.dev <66074802+Dev-Beom@users.noreply.github.com>
2023-06-26 01:08:00 +0900

    카드 / 문양 데이터 추가

--- src/main/kotlin/com/play/poker/value/Card.kt
+++ src/main/kotlin/com/play/poker/value/Card.kt
@@ -1,3 +1,17 @@
 package com.play.poker.value
 
-data class Card()
+data class Card(
+    val number: Int,
+    val suit: Suit
+) {
+    val display: String
+        get() {
+            return when (number) {
+                1 -> "A"
+                11 -> "J"
+                12 -> "Q"
+                13 -> "K"
+                else -> number.toString()
+            }
+        }
+}
--- src/main/kotlin/com/play/poker/value/Suit.kt
+++ src/main/kotlin/com/play/poker/value/Suit.kt
@@ -1,4 +1,30 @@
 package com.play.poker.value
 
-enum class Suit {
+import java.awt.Color
+
+enum class Suit(
+    val symbol: String,
+    val displayName: String,
+    val color: Color
+) {
+    SPADE(
+        "♠",
+        displayName = "스페이드",
+        color = Color.black
+    ),
+    HEART(
+        symbol = "♥",
+        displayName = "하트",
+        color = Color.red
+    ),
+    DIAMOND(
+        symbol = "♦",
+        displayName = "다이아몬드",
+        color = Color.red
+    ),
+    CLUB(
+        symbol = "♣",
+        displayName = "클로바",
+        color = Color.black
+    )
 }
