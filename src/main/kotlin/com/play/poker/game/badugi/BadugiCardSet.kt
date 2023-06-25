b34817668fbe67529e4e1efaa7350742726b0e32
benn.dev <66074802+Dev-Beom@users.noreply.github.com>
2023-06-26 02:00:25 +0900

    바둑이 코어 to 카드셋 책임 분리

--- src/main/kotlin/com/play/poker/game/badugi/BadugiCardSet.kt
+++ src/main/kotlin/com/play/poker/game/badugi/BadugiCardSet.kt
@@ -1,26 +1,93 @@
 package com.play.poker.game.badugi
 
+import com.play.poker.util.Combination
 import com.play.poker.value.Card
 
 class BadugiCardSet(
-    private val cards: List<Card>
+    private val cards: List<Card>,
 ) {
-    companion object {
-        private const val FIXED_CARD_SIZE = 4
-
-        fun from(cards: List<Card>): BadugiCardSet {
-            check(cards.size == FIXED_CARD_SIZE) { "badugi card set size exception." }
-            return BadugiCardSet(cards = cards)
+    val genealogy: BadugiGenealogy by lazy {
+        when {
+            this.isGolf() -> BadugiGenealogy.GOLF
+            this.isSecond() -> BadugiGenealogy.SECOND
+            this.isThird() -> BadugiGenealogy.THIRD
+            this.isMade() -> BadugiGenealogy.MADE
+            this.isBase() -> BadugiGenealogy.BASE
+            this.isTwoBase() -> BadugiGenealogy.TWO_BASE
+            this.isNoneTop() -> BadugiGenealogy.NONE_TOP
+            else -> throw Exception("badugi card set get genealogy exception.")
         }
-    }
-
-    fun orderedCards(): List<Card> {
-        check(cards.size == FIXED_CARD_SIZE) { "badugi card set size exception." }
-        return cards.sortedBy { it.number }
     }
 
     override fun toString(): String {
         check(cards.size == FIXED_CARD_SIZE) { "badugi card set size exception." }
         return cards.joinToString(separator = "") { it.display }
+    }
+
+    private fun BadugiCardSet.isGolf(): Boolean {
+        return this.isMade() && this.matchOf("A234")
+    }
+
+    private fun BadugiCardSet.isSecond(): Boolean {
+        println(this.cards.toString())
+        return this.isMade() && this.matchOf("A235")
+    }
+
+    private fun BadugiCardSet.isThird(): Boolean {
+        return this.isMade() && this.matchOf("A245")
+    }
+
+    private fun BadugiCardSet.isMade(): Boolean {
+        return this.cards.isAllDiffNumber() && this.cards.isAllDiffSuit()
+    }
+
+    private fun BadugiCardSet.isBase(): Boolean {
+        return !this.isMade() && this.cards.isDiffNumberAndSuitFor(3)
+    }
+
+    private fun BadugiCardSet.isTwoBase(): Boolean {
+        return !this.isMade() && this.cards.isDiffNumberAndSuitFor(2)
+    }
+
+    private fun BadugiCardSet.isNoneTop(): Boolean {
+        return this.cards.isAllSameNumber() || this.cards.isAllSameSuit()
+    }
+
+    private fun List<Card>.isDiffNumberAndSuitFor(countOfCard: Int): Boolean {
+        val combinations = Combination.combinations(this, countOfCard)
+        for (combination in combinations) {
+            if (combination.isAllDiffNumber() && combination.isAllDiffSuit()) return true
+        }
+        return false
+    }
+
+    private fun List<Card>.isAllDiffNumber(): Boolean {
+        return this.none { card -> this.filter { it.number == card.number }.size != 1 }
+    }
+
+    private fun List<Card>.isAllDiffSuit(): Boolean {
+        return this.none { card -> this.filter { it.suit == card.suit }.size != 1 }
+    }
+
+    private fun List<Card>.isAllSameNumber(): Boolean {
+        return this.count { current -> current.number == this.first().number } == this.count()
+    }
+
+    private fun List<Card>.isAllSameSuit(): Boolean {
+        return this.count { current -> current.suit == this.first().suit } == this.count()
+    }
+
+    private fun BadugiCardSet.matchOf(string: String): Boolean {
+        return this.toString() == string
+    }
+
+    companion object {
+        private const val FIXED_CARD_SIZE = 4
+
+        fun from(cards: List<Card>): BadugiCardSet {
+            check(cards.size == FIXED_CARD_SIZE) { "badugi card set size exception." }
+            val sortedCards = cards.sortedBy { it.number }
+            return BadugiCardSet(cards = sortedCards)
+        }
     }
 }
--- src/test/kotlin/com/play/poker/game/badugi/BadugiCardSetTest.kt
+++ src/test/kotlin/com/play/poker/game/badugi/BadugiCardSetTest.kt
@@ -1,27 +1,23 @@
 package com.play.poker.game.badugi
 
 import com.play.poker.util.dsl.at
-import com.play.poker.value.Card
 import com.play.poker.value.Suit.*
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 
-class BadugiCoreTest {
+class BadugiCardSetTest {
     private val badugiCore = BadugiCore()
 
     @Test
     fun `getGenealogy - GOLF`() {
-        val cardSet = BadugiCardSet.from(
-            cards = listOf(CLUB at 1, HEART at 2, DIAMOND at 3, SPADE at 4)
-        )
         assertDoesNotThrow(
             {
-                val result = badugiCore.getGenealogy(cardSet)
-                assertThat(result).isSameAs(BadugiGenealogy.GOLF)
+                val cardSet = BadugiCardSet.from(
+                    cards = listOf(CLUB at 1, HEART at 2, DIAMOND at 3, SPADE at 4)
+                )
+                assertThat(cardSet.genealogy).isSameAs(BadugiGenealogy.GOLF)
             },
             "골프는 카드 4장의 무늬와 숫자가 모든 가른 경우인 메이드 중에서 가장 높은 경우이다."
         )
@@ -29,13 +25,12 @@ class BadugiCoreTest {
 
     @Test
     fun `getGenealogy - SECOND`() {
-        val cardSet = BadugiCardSet.from(
-            cards = listOf(CLUB at 1, HEART at 2, DIAMOND at 3, SPADE at 5)
-        )
         assertDoesNotThrow(
             {
-                val result = badugiCore.getGenealogy(cardSet)
-                assertThat(result).isSameAs(BadugiGenealogy.SECOND)
+                val cardSet = BadugiCardSet.from(
+                    cards = listOf(CLUB at 1, HEART at 2, DIAMOND at 3, SPADE at 5)
+                )
+                assertThat(cardSet.genealogy).isSameAs(BadugiGenealogy.SECOND)
             },
             "세컨드는 카드 4장의 무늬와 숫자가 모든 가른 경우인 메이드 중에서 두번째로 높은 경우이다."
         )
@@ -43,13 +38,12 @@ class BadugiCoreTest {
 
     @Test
     fun `getGenealogy - THIRD`() {
-        val cardSet = BadugiCardSet.from(
-            cards = listOf(CLUB at 1, HEART at 2, DIAMOND at 4, SPADE at 5)
-        )
         assertDoesNotThrow(
             {
-                val result = badugiCore.getGenealogy(cardSet)
-                assertThat(result).isSameAs(BadugiGenealogy.THIRD)
+                val cardSet = BadugiCardSet.from(
+                    cards = listOf(CLUB at 1, HEART at 2, DIAMOND at 4, SPADE at 5)
+                )
+                assertThat(cardSet.genealogy).isSameAs(BadugiGenealogy.THIRD)
             },
             "써드는 카드 4장의 무늬와 숫자가 모든 가른 경우인 메이드 중에서 세번째로 높은 경우이다."
         )
@@ -57,13 +51,12 @@ class BadugiCoreTest {
 
     @Test
     fun `getGenealogy - MADE`() {
-        val cardSet = BadugiCardSet.from(
-            cards = listOf(SPADE at 1, CLUB at 4, DIAMOND at 5, HEART at 6)
-        )
         assertDoesNotThrow(
             {
-                val result = badugiCore.getGenealogy(cardSet)
-                assertThat(result).isSameAs(BadugiGenealogy.MADE)
+                val cardSet = BadugiCardSet.from(
+                    cards = listOf(SPADE at 1, CLUB at 4, DIAMOND at 5, HEART at 6)
+                )
+                assertThat(cardSet.genealogy).isSameAs(BadugiGenealogy.MADE)
             },
             "메이드는 카드 4장의 무늬와 숫자가 모두 다른 경우이다."
         )
@@ -71,13 +64,12 @@ class BadugiCoreTest {
 
     @Test
     fun `getGenealogy - BASE`() {
-        val cardSet = BadugiCardSet.from(
-            cards = listOf(SPADE at 1, CLUB at 4, DIAMOND at 5, DIAMOND at 6)
-        )
         assertDoesNotThrow(
             {
-                val result = badugiCore.getGenealogy(cardSet)
-                assertThat(result).isSameAs(BadugiGenealogy.BASE)
+                val cardSet = BadugiCardSet.from(
+                    cards = listOf(SPADE at 1, CLUB at 4, DIAMOND at 5, DIAMOND at 6)
+                )
+                assertThat(cardSet.genealogy).isSameAs(BadugiGenealogy.BASE)
             },
             "베이스는 메이드가 아닌 패 중에서 1장을 제외하면 나머지 3장의 무늬와 숫자가 모두 다른 경우이다."
         )
@@ -85,13 +77,12 @@ class BadugiCoreTest {
 
     @Test
     fun `getGenealogy - TWO_BASE`() {
-        val cardSet = BadugiCardSet.from(
-            cards = listOf(SPADE at 1, CLUB at 1, DIAMOND at 5, DIAMOND at 6)
-        )
         assertDoesNotThrow(
             {
-                val result = badugiCore.getGenealogy(cardSet)
-                assertThat(result).isSameAs(BadugiGenealogy.TWO_BASE)
+                val cardSet = BadugiCardSet.from(
+                    cards = listOf(SPADE at 1, CLUB at 1, DIAMOND at 5, DIAMOND at 6)
+                )
+                assertThat(cardSet.genealogy).isSameAs(BadugiGenealogy.TWO_BASE)
             },
             "투베이스는 메이드가 아닌 패 중에서 2장을 제외하면 나머지 2장의 무늬와 숫자가 모두 다른 경우이다."
         )
@@ -99,16 +90,16 @@ class BadugiCoreTest {
 
     @Test
     fun `getGenealogy - NONE`() {
-        val cardSet1 = BadugiCardSet.from(
-            cards = listOf(SPADE at 1, SPADE at 2, SPADE at 3, SPADE at 4)
-        )
-        val cardSet2 = BadugiCardSet.from(
-            cards = listOf(SPADE at 1, CLUB at 1, DIAMOND at 1, HEART at 1)
-        )
         assertDoesNotThrow(
             {
-                val result1 = badugiCore.getGenealogy(cardSet1)
-                val result2 = badugiCore.getGenealogy(cardSet2)
+                val cardSet1 = BadugiCardSet.from(
+                    cards = listOf(SPADE at 1, SPADE at 2, SPADE at 3, SPADE at 4)
+                )
+                val cardSet2 = BadugiCardSet.from(
+                    cards = listOf(SPADE at 1, CLUB at 1, DIAMOND at 1, HEART at 1)
+                )
+                val result1 = cardSet1.genealogy
+                val result2 = cardSet2.genealogy
                 assertThat(result1).isSameAs(BadugiGenealogy.NONE_TOP)
                 assertThat(result2).isSameAs(BadugiGenealogy.NONE_TOP)
             },
